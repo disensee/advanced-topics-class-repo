3cab2a1fb428dd55db93cb435b2eec79739ee349
disensee <isenseed@students.westerntc.edu>
2020-01-28 07:36:25 -0600

    Worked on date utils

--- javascript/date-utils/step01/date-utils.js
+++ javascript/date-utils/step01/date-utils.js
@@ -31,8 +31,15 @@ dateUtils.format = function(){
 * @param {Date} birthDate		The date to check. 	
 * @return {boolean}				True if the date is more than 18 years ago. False if not.
 */
-dateUtils.isOldEnoughToVote = function(birthDate){
-
+dateUtils.isOldEnoughToVote = function(birthDate){ 
+    var todayInMs = new Date().getTime();
+    var birthDayInMs = birthDate.getTime();
+    var test = todayInMs - birthDayInMs;
+    if(todayInMs - birthDayInMs >= 568025136000){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
