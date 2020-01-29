96f8bc5c4eea8aa08fd5e879cdfd9f2db8893903
disensee <isenseed@students.westerntc.edu>
2020-01-28 21:20:36 -0600

    Finished dateUtils assignment

--- javascript/date-utils/step01/date-utils.js
+++ javascript/date-utils/step01/date-utils.js
@@ -4,23 +4,57 @@ var dateUtils = dateUtils || {};
 * Formats a date to look like this: Sunday January 11, 2018
 * @method format
 */
-dateUtils.format = function(){
+// dateUtils.format = function(){
 
-    var date = new Date();
+//     var date = new Date();
 
-    var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//     var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+//     var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     
-    var monthName = monthNames[date.getMonth()];
+//     var monthName = monthNames[date.getMonth()];
+//     var dayName = dayNames[date.getDay()];
+
+//     var day = date.getDate();
+//     var year = date.getFullYear();
+    
+//     var formattedDate = dayName + " " + monthName + " " + day + ", " + year;
+    
+//     alert(formattedDate);
+  
+// }
+
+dateUtils.formatDay = function(){
+    var date = new Date();
+    var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var dayName = dayNames[date.getDay()];
 
-    var day = date.getDate();
-    var year = date.getFullYear();
-    
-    var formattedDate = dayName + " " + monthName + " " + day + ", " + year;
-    
-    alert(formattedDate);
-  
+    return dayName;
+}
+
+dateUtils.formatMonth = function(){
+    var date = new Date();
+    var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var monthName = monthNames[date.getMonth()];
+
+    return monthName;
+}
+
+dateUtils.getDayNumber = function(){
+    var today = new Date();
+    var dayNumber = today.getDate();
+
+    return dayNumber;
+}
+
+dateUtils.getYearNumber = function(){
+    var date = new Date();
+    var yearNumber = date.getFullYear();
+
+    return yearNumber;
+}
+
+dateUtils.format = function(){
+    alert("Today is " + dateUtils.formatDay() + " " + dateUtils.formatMonth() + " " + dateUtils.getDayNumber() + ", " + dateUtils.getYearNumber());
 }
 
 
