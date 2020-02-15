d5f998c69ee31b11d8ba316327fc3460fd83e386
disensee <isenseed@students.westerntc.edu>
2020-02-14 19:53:53 -0600

    Worked on simple module step 2. Finished part2. Questions still need to be answered

--- javascript/simple-module/step02/js/SimpleModule.js
+++ javascript/simple-module/step02/js/SimpleModule.js
@@ -2,11 +2,9 @@ class SimpleModule{
 
     constructor(options){ 
         console.log(options);
-        //////////////////////////////////////////
-	    // INSTANCE VARIABLES/////////////////////
-	    //////////////////////////////////////////
-        this.container = this.options.container || null;
-        this.firstName = this.options.firstName || null;
+        //Instance variables
+        this.container = options.container || null;
+        this.firstName = options.firstName || null;
     
         this.template = '<div class="simple-module"> \
                             <div class="first-name"></div> \
@@ -19,6 +17,34 @@ class SimpleModule{
         // UI variables
         this.divFirstName;
         this.btnIncrement;
+
+        this.initialize();
+    }
+
+
+    initialize(){
+        // inject the template into the page
+		this.container.innerHTML = this.template;
+		
+		// initalize UI variables
+		this.divFirstName = this.container.querySelector(".first-name");
+		this.btnIncrement = this.container.querySelector(".increment");
+
+		// display the first name in the first name div
+		this.divFirstName.innerHTML = "Hello " + this.firstName;
+
+        // hook up event listeners
+        let self = this;
+		this.btnIncrement.addEventListener("click", function(){
+			self.pressCount++;
+			self.update();
+		});
+    }
+
+    update(){
+        this.msg = this.firstName + " you have pressed the button " + this.pressCount + " time(s)!";
+        this.divFirstName.innerHTML = this.msg;
+        return this.msg;
     }
 
     
