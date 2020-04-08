25493921c0da9249194650612fe279d62e4f90bb
disensee <isenseed@students.westerntc.edu>
2020-04-08 11:21:55 -0500

    In class restful api activity - 4/8

--- web-services/contacts/index.php
+++ web-services/contacts/index.php
@@ -1,4 +1,8 @@
 <?php
+include_once("../includes/config.inc.php");
+include_once("../includes/dataaccess/ContactDataAccess.inc.php");
+
+$da = new ContactDataAccess(get_link());
 
 $method = $_SERVER['REQUEST_METHOD'];
 $url_path = $_GET['url_path'] ?? ""; // the url_path querystring gets appended per the .htaccess file
@@ -24,12 +28,29 @@ switch($method){
   case "GET":
     
     if(empty($url_path)){
-      die("GET REQUEST for contacts/");
+      //die("GET REQUEST for contacts/");
+      $contacts = $da->getAll();
+      $json = json_encode($contacts);
+      header("Content-Type: application/json");
+      echo($json);
+      die();
     }else if(preg_match('/^([0-9]*\/?)$/', $url_path, $matches)){ 
       $contact_id = $matches[1];
-      die("GET REQUEST for contacts/" . $contact_id);
+      //die("GET REQUEST for contacts/" . $contact_id);
+      $contact = $da->getById($contact_id);
+      if($contact == false){
+        header('HTTP/1.1 404 Not Found', true, 404);
+        die();
+      }else{
+        $json = json_encode($contact);
+        header("Content-Type: application/json");
+        echo($json);
+        die();
+      }
     }else{
-      die("INVALID REQUEST to ". $url_path);
+      //die("INVALID REQUEST to ". $url_path);
+      header('HTTP/1.1 400 - Invalid Request', true, 400);
+      die();
     }
     
     break;
@@ -37,7 +58,21 @@ switch($method){
   case "POST":
 
     if(empty($url_path)){
-      die("POST REQUEST to insert contacts/");    
+      //die("POST REQUEST to insert contacts/");
+      $requestBody = file_get_contents("php://input");
+      $assoc = json_decode($requestBody, TRUE);
+      $contact = new Contact($assoc);
+      
+      if($contact->isValid() == false){
+        header('HTTP/1.1 400 - INVALID REQUEST - INVALID CONTACT DATA', true, 400);
+      }
+      
+      $contact = $da->insert($contact);
+      
+      $json = json_encode($contact);
+      header("Content-Type: application/json");
+      echo($json);
+      die();
     }else{
       die("INVALID POST REQUEST TO: $url_path");
     }
@@ -49,7 +84,16 @@ switch($method){
     if(preg_match('/^([0-9]*\/?)$/', $url_path, $matches)){
         
       $contact_id = $matches[1];
-      die("PUT REQUEST to update contacts/" . $contact_id);
+      //die("PUT REQUEST to update contacts/" . $contact_id);
+      $requestBody = file_get_contents("php://input");
+      $assoc = json_decode($requestBody, TRUE);
+      $contact = new Contact($assoc);
+        //TODO: Validate the contact before updating
+      $contact = $da->update($contact);
+      $json = json_encode($contact);
+      header("Content-Type: application/json");
+      echo($json);
+      die();
     }else{
       die("INVALID PUT REQUEST TO " . $url_path);
     }
@@ -61,9 +105,17 @@ switch($method){
     if(preg_match('/^([0-9]*\/?)$/', $url_path, $matches)){
       // Get the id of the task from the URL (via regular expression)
       $contact_id = $matches[1];
-      die("DELETE REQUEST to delete contacts/" . $contact_id);
+      $result = $da->delete($contact_id);
+      //die("DELETE REQUEST to delete contacts/" . $contact_id);
+      if($result){
+        header('HTTP/1.1 200', true, 200);
+        die();
+      }else{
+        header('HTTP/1.1 500 - UNABLE TO DELETE CONTACT', true, 500);
+        die();
+      }
     }else{
-      die("INVALID DELETE REQUEST TO " . $url_path);
+      //die("INVALID DELETE REQUEST TO " . $url_path);
     }
 
     break;
